3439d59ea4e9e9b47faed46136aaf541323a1c6e
Kirill Kuznetsov <kuznechik.86@mail.ru>
2020-12-01 20:27:30 +0300

    Initial commit

---
+++ manager-counters/src/main/java/com/kuznetsovka/managercounters/ManagerCountersApplication.java
@@ -0,0 +1,13 @@
+package com.kuznetsovka.managercounters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerCountersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerCountersApplication.class, args);
+	}
+
+}
---
+++ manager-counters/src/test/java/com/kuznetsovka/managercounters/ManagerCountersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuznetsovka.managercounters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerCountersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
